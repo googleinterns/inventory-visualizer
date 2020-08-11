f5efdc5165d188ed2f3954e6b06749091b36fa1c
sophiepeneva <sophiepeneva@google.com>
2020-08-11 17:22:48 +0200

    Readibility changes

--- backend/filters/data_filter.py
+++ backend/filters/data_filter.py
@@ -13,8 +13,7 @@ class DataFilter:
         self.filters = self.get_only_supported_filters(requested_filters)
 
     def get_only_supported_filters(self, requested_filters):
-        return {supported: requested_filters[supported] for supported in self.filterDict.keys() if
-                supported in requested_filters}
+        return {k: v for k, v in requested_filters.items() if k in self.filterDict}
 
     def filter(self, data):
         for key, value in self.filters.items():
@@ -30,9 +29,22 @@ class DataFilter:
         return self.filter_by_key(data, devices, 1)
 
     @staticmethod
-    def filter_by_key(data, values, key):
-        tuples = list(data.keys())
-        keys = [i for i in tuples if i[key] in values]
+    def filter_by_key(data, values, key_index):
+        """
+        This function filters a dictionary that has tuples for keys so that all keys have
+        a value in position key_index that exists in a given set of values
+
+        Parameters:
+        data (dict): A dict that has (country, device) tuples for keys and data_pb2.SegmentData for values
+        values (list): Allowed values for the tuple values in a given position
+        key_index (int): The position in the tuples to look for the values
+
+        Returns:
+        dict: A dictionary that has (country, devices) as keys where all the values in the tuples in position
+        key_index exist in the list values
+
+        """
+        keys = [tuple for tuple in data.keys() if tuple[key_index] in values]
         return {key: data[key] for key in keys}
 
     def filter_by_start_date(self, data, start_date):
@@ -42,12 +54,14 @@ class DataFilter:
         return self.filter_by_date(data, end_date, operator.le)
 
     @staticmethod
-    def filter_by_date(data, bound_date, relation):
-        for key, segment in data.items():
-            keys = [key for key in range(len(segment.dates)) if relation(segment.dates[key].seconds, int(bound_date))]
+    def filter_by_date(data, bound_date, relation_comparator):
+        new_data = {}
+        for index, segment in data.items():
+            keys = [i for i in range(len(segment.dates)) if
+                    relation_comparator(segment.dates[i].seconds, int(bound_date))]
             dates = [segment.dates[key] for key in keys]
             inventory_volumes = [segment.inventory_volumes[key] for key in keys]
             filtered_segment = data_pb2.SegmentData(country=segment.country, device=segment.device, dates=dates,
                                                     inventory_volumes=inventory_volumes)
-            data[key] = filtered_segment
-        return data
+            new_data[index] = filtered_segment
+        return new_data
