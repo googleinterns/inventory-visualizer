d6597b20a3d768a3e0e8d3f11c3e732d061f37dd
sophiepeneva <sophiepeneva@google.com>
2020-08-18 17:46:57 +0200

    Draw charts after file upload

--- frontend/src/app/api.service.ts
+++ frontend/src/app/api.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
 
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpParams} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {environment} from '../environments/environment';
 
@@ -12,6 +12,28 @@ export class ApiService {
   constructor(private http: HttpClient) {
   }
 
+  getSegmentedData(filename, page = 0, perPage = 8, filters = null): Observable<object> {
+
+    let params = new HttpParams()
+      .set('page', page.toString())
+      .set('per_page', perPage.toString());
+    params = this.buildFilter(filters, params);
+    return this.http.get(environment.apiRest + 'data/' + filename, {params});
+  }
+
+  getComparisonData(filename, filename2, page = 0, perPage = 8, filters = null): Observable<object> {
+
+    let params = new HttpParams()
+      .set('page', page.toString())
+      .set('per_page', perPage.toString());
+    params = this.buildFilter(filters, params);
+    return this.http.get(environment.apiRest + 'compare/' + filename + '/' + filename2, {params});
+  }
+
+  getErrorData(filename, filename2): Observable<object> {
+    return this.http.get(environment.apiRest + 'error/' + filename + '/' + filename2);
+  }
+
   uploadFile(files): Observable<object> {
     const formData: FormData = new FormData();
     formData.append('uploaded_data', files[0], files[0].name);
@@ -19,7 +41,30 @@ export class ApiService {
   }
 
   deleteFile(files): void {
-    this.http.delete( environment.apiRest + 'file' + '?filename=' + files[0].name);
+    const params = new HttpParams()
+      .set('filename', files[0].name);
+    this.http.delete(environment.apiRest + 'file', {params});
+  }
+
+  buildFilter(filters, params): HttpParams {
+    if (!filters) {
+      return params;
+    }
+    if (filters.device != null) {
+      params = params.set('device', filters.device);
+    }
+    if (filters.countries && filters.countries !== [] && filters.countries.length !== 0) {
+      params = params.set('country', filters.countries.join([',']));
+    }
+    if (filters.fromDate != null) {
+      const date = new Date(filters.fromDate);
+      params = params.set('start_date', (date.getTime() / 1000).toString());
+    }
+    if (filters.toDate != null) {
+      const date = new Date(filters.toDate);
+      params = params.set('end_date', (date.getTime() / 1000).toString());
+    }
+    return params;
   }
 }
 
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,17 +1,22 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {HttpClientModule} from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {GoogleChartsModule} from 'angular-google-charts';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { GoogleChartsModule } from 'angular-google-charts';
 import {ApiService} from './api.service';
-import {InfiniteScrollModule} from 'ngx-infinite-scroll';
-import {NgxSpinnerModule} from 'ngx-spinner';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+import { GoogleChartsComponent } from './google-charts/google-charts.component';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { NgSelectModule } from '@ng-select/ng-select';
 
 @NgModule({
   declarations: [
     AppComponent,
+    GoogleChartsComponent
   ],
   imports: [
     BrowserModule,
@@ -20,10 +25,12 @@ import {NgxSpinnerModule} from 'ngx-spinner';
     HttpClientModule,
     InfiniteScrollModule,
     NgxSpinnerModule,
+    NgbModule,
+    FormsModule,
+    NgSelectModule
   ],
   providers: [ApiService],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule { }
 
