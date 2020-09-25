9e6215222ab1f9b7eca6c20109d890b8e29329de
sophiepeneva <sophiepeneva@google.com>
2020-09-25 17:23:58 +0200

    Add comment to explain error patterns

--- backend/classifiers/error_patter_classifier.py
+++ backend/classifiers/error_patter_classifier.py
@@ -6,6 +6,19 @@ from utils.date_util import seconds_to_timestamp
 
 
 def get_error_patterns(segmented_errors, threshold):
+    """
+
+    This method takes a list of segmented errors and an error threshold as parameters. The
+    list of segmented errors essentially tells us the different error values for different dates
+    per segment. This method then returns the odds of en error larger than the given threshold
+    per every day. Where for a single date, the odds of an error are equalt to the number of segemnts
+    with an error larger than the threshold for the given date divided by the total number of segments
+    that provide error information for that date. 
+
+    :param segmented_errors: list The list contains data_pb2.SegmentedDataError
+    :param threshold: float
+    :return: data_pb2.ErrorPatternResponse Structure that contains the odds of an error per date
+    """
     large_errors_per_date = {}
 
     for segmented_error in segmented_errors:
